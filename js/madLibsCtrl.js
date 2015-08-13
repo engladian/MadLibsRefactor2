327eb34ae0427b93f6530d3d591392076dc704b4
engladian <simon.franklin2@cigna.com>
2015-08-13 18:49:29 +1200

    First Cut

---
+++ js/madLibsCtrl.js
@@ -0,0 +1,12 @@
+﻿angular.module('appMadLibs', [])
+    .controller('madLibsCtrl', function ($scope) {
+        $scope.name = '';
+        $scope.jobTitle = '';
+        $scope.tediuosTask = '';
+        $scope.dirtyTask = '';
+        $scope.celeb = '';
+        $scope.uselessSkill = '';
+        $scope.obnoxiousCeleb = '';
+        $scope.hugeNumber = 1;
+        $scope.adjective = '';
+    });
